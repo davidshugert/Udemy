d96627de79ec8e270ac2fd572773c33fa31baf9e
David Shugert <shugertdavid@gmail.com>
2019-05-20 16:01:10 -0500

    Final commit of the course

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,39 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner'
+import useLocation from './useLocation'
 
-class App extends React.Component {
-
-    state = { lat: null, errorMessage: '' }
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            pos => this.setState({ lat: pos.coords.latitude }),
-            err => this.setState({ errorMessage: err.message })
-        )
+const App = () => {
+    const [lat, errorMessage] = useLocation()
+    let content
+    if (errorMessage) {
+        content = <div>Error:{errorMessage}</div>
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat} />
+    } else {
+        content = <Spinner message='Please Accept Location Request' />
     }
-    componentDidUpdate() {
-        console.log('it updated');
-    }
+    return <div className='border red'>{content}</div>
 
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
-        }
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-
-        return <Spinner message='Please Accept Location Request' />
-    }
-    render() {
-        return (
-            <div className='border red'>
-                {this.renderContent()}
-            </div>
-        )
-
-    }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"))
--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { fetchStream } from '../../actions'
-import { loadPartialConfig } from '@babel/core';
+import flv from 'flv.js'
 
 class StreamShow extends React.Component {
+    constructor(props) {
+        super(props)
+        this.videoRef = React.createRef()
+    }
+
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id)
+        const { id } = this.props.match.params.id
+        this.props.fetchStream(id)
+        this.buildPlayer()
+
+
+    }
+    componentDidUpdate() {
+        this.buildPlayer()
+    }
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return
+        }
+        const { id } = this.props.match.params.id
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load()
     }
 
     render() {
-        if(!this.props.stream){
+        if (!this.props.stream) {
             return <div>Loading</div>
         }
-        const {title,description} = this.props.stream
+        const { title, description } = this.props.stream
         return (
             <div>
-                <h5>{title}</h5>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls={true} />
+                <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
         )
